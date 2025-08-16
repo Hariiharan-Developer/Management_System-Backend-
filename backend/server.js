31411100b8ab217fb68a6ce3d23b475805514b0e
Hariharan.s <hreehs1428@gmail.com>
2025-08-16 14:31:38 +0530

    Api Created Each Parts

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const port =process.env.PORT || 8000
 
 const app= express()
 
+app.use('/api/goals',require('./routes/goal.route'))
+
 app.listen(port,()=>{
-    console.log(`server listening on the port: http://localhost: ${port}`)
+    console.log(`server listening on the port: http://localhost:${port}`)
 })
