baa17e3c339f4556d4859c1a6ffec09f9b076610
Hariharan.s <hreehs1428@gmail.com>
2025-08-16 13:47:43 +0530

    Inital commit for setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('hello')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port =process.env.PORT || 8000
+
+const app= express()
+
+app.listen(port,()=>{
+    console.log(`server listening on the port: http://localhost: ${port}`)
+})
